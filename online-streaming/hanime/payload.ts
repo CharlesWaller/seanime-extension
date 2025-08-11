95bf74233456d767808b29139d07fd0720ae34e0
TropicalFrog3 <6216365@etu.cegepsth.qc.ca>
2025-08-11 13:47:14 -0400

    small changes for hanime payload

--- online-streaming/hanime/payload.ts
+++ online-streaming/hanime/payload.ts
@@ -1,5 +1,5 @@
-/// <reference path=".onlinestream-provider.d.ts" />
-/// <reference path="./core.d.ts" />
+/// <reference path="../external/.onlinestream-provider.d.ts" />
+/// <reference path="../external/core.d.ts" />
 
 // ---------- Types ----------
 type RawSearchResult = {
